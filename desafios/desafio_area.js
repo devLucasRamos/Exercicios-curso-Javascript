fb8973c61076326eb106406888891647933341f2
Lucas Ramos <87498874+devLucasRamos@users.noreply.github.com>
2021-10-05 13:06:55 -0300

    desafios iniciais

---
+++ desafios/desafio_area.js
@@ -0,0 +1,13 @@
+// PI(3.141592) * raio(10) * raio(10)
+// Math.PI
+
+const pi = 3.141592;
+let raio = 10;
+let raio2 = raio * raio;
+let areaCirc = pi * raio2;
+let areaCircMath = Math.PI * raio2;
+console.log(areaCirc);
+console.log(areaCircMath);
+console.log(Math.PI * raio2);
+console.log(pi * raio * raio);
+console.log(Math.PI * raio * raio);
---
+++ desafios/desafio_troca.js
@@ -0,0 +1,14 @@
+let a = 7;
+let b = 94;
+
+// trocar o valor de uma var pela outra, depois da troca, a = 94, b = 7
+
+/*let temp = a;
+a = b;
+b = temp;
+OU
+*/
+
+[a,b] = [b,a]
+console.log(a);
+console.log (b);
